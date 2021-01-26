c819baf463fab30d42c915ee1746a62fb5df5042
bjsec <ayres.sailing@gmail.com>
2021-01-26 20:13:21 -0300

    Alterações da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import QuizLogo from '../src/components/QuizLogo'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GitHubCorner from '../src/components/GitHubCorner'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -25,16 +27,41 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useSTATE', name, setName);
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>BJSECURITY</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>{db.title}</h1>
+            <h1>Quiz sobre Segurança Cibernética</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('fazendo uma submissao por meio do react');                        
+            }}
+            >
+              <input 
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value)
+                  //state
+                  //name = infosDoEvento.target.value 
+                  setName(infosDoEvento.target.value)
+                }}             
+                placeholder="Digite teu nome" 
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogue  {name} !
+              </button>
+            </form>
+
           </Widget.Content>
         </Widget>
 
